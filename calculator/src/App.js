7432f04b01b07eb4d884423d36151b0dfbf1000b
faraaz <ugp@Faraaz-MacBook.local>
2019-09-09 23:18:32 -0700

    Basic design

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -4,21 +4,97 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-md btn-block">
+              7
+			      </button>
+          </div>
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-md btn-block">
+              8
+			      </button>
+          </div>
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-md btn-block">
+              9
+			      </button>
+          </div>
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-md btn-block">
+              /
+			      </button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-block">
+              4
+			      </button>
+          </div>
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-block">
+              5
+			      </button>
+          </div>
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-block">
+              6
+			      </button>
+          </div>
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-block">
+              *
+			      </button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-block">
+              1
+			      </button>
+          </div>
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-block">
+              2
+			      </button>
+          </div>
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-block">
+              3
+			      </button>
+          </div>
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-block">
+              -
+			      </button>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-block">
+              .
+			      </button>
+          </div>
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-block">
+              0
+			      </button>
+          </div>
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-block">
+              =
+			      </button>
+          </div>
+          <div class="col-md-3">
+            <button type="button" class="btn btn-success btn-block">
+              +
+			      </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
